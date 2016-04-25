c2249b9dc65e6833b527a3f94c795ad9d6026d8f
Rémi Bèges <remi.beges@gmail.com>
2016-04-25 14:34:35 +0200

    cleanup

--- run.py
+++ run.py
@@ -3,62 +3,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import integrate
 
-sparams_file = "sensor.csv"
-voltage_file = "passthrough_sensor_10V.csv"
+sparams_file = "sensor_sparams.csv"
+voltage_file = "transient_waveform_10V.csv"
+voltage_sampling_period = 8e-12 # Scope sample frequency
+reference_file = "reference_i_measurement.csv"
 
+# Load S-parameters measurement
 d = load(sparams_file,' |\t', skiprows=3)
 freqs = d[:,0]
 mag = d[:,1]
 phase = d[:,2]
 
-## SENSOR PROCESSING
+# 1st Method : frequency domain
 # Add a frequency point at 0
 freqs = np.insert(freqs, 0, freqs[0]/1e5)
 mag = np.insert(mag, 0, mag[0] - 100)
 phase = np.insert(phase, 0, phase[0])
-
+# Convert phase to 0
 phase = np.radians(phase)
 
+# Load transient voltage waveform to be post-processed
 d = load(voltage_file, ',', skiprows=5)
 time = d[:,3]
 voltage = d[:,4]
 
+# Perform post-processing of the transient voltage waveform
+processed_time, processed_voltage = convert(freqs, mag, phase, time, voltage, sampling=voltage_sampling_period)
+processed_voltage /= 20
+
+# 2nd Method : Integration
+# Perform integration-based reconstitution method of the current waveform
 integral = integrate.cumtrapz(voltage, time, initial=0)
-integral *= 3e9
+# Apply gain
+integral *= 8e8
 
-analyse("Time", time)
-analyse("integral", integral)
-print(integral)
+# 3rd Method : original TLP measurmement of injected current
+# Load reference current measurement (TLP measurement)
+d = load(reference_file, '\t', skiprows=1)
+ref_time = d[:,0]
+ref_current = d[:,2]
 
-#plt.semilogx(freqs, mag, freqs, phase)
-#plt.show()
-
-# Test data
-#time = np.arange(0,3,0.0001)
-#voltage = np.sin(2 * np.pi * 10 * time + np.pi)
-
-# Test data 2
-#N=600
-#T = 1.0 / 800.0
-#time = np.linspace(0.0, N*T, N)
-#voltage = np.sin(50.0 * 2.0*np.pi*time) + 0.5*np.sin(80.0 * 2.0*np.pi*time)
-
-analyse("S params frequency points", freqs)
-
-#plt.plot(time, voltage, label="transient waveform")
-#plt.show()
-
-# Process values
-#convert(sparams[:,0], sparams[:,3], v_wvf[:,3], v_wvf[:,4],sampling=8e-12)
-processed_time, processed_voltage = convert(freqs, mag, phase, time, voltage, sampling=8e-12)
 
 # Final plot
-original, = plt.plot(time, voltage, label="original curve")
+original, = plt.plot(ref_time, ref_current, label="original curve")
 trap, = plt.plot(time, integral, label="integral method")
-print(type(processed_time[0]))
-print(type(processed_voltage[0]))
 final, = plt.plot(processed_time, processed_voltage, label="frequency method")
 plt.legend(handles=[original,trap,final])
 plt.show()
-
-analyse("Post processed voltage waveform", processed_voltage)
